16c63c2f4201ce5d29f4473952cd184859e9b3ce
sahar085 <37782726+shfhm@users.noreply.github.com>
2018-05-19 17:31:41 -0400

    Update CVEid.py

--- CVEid.py
+++ CVEid.py
@@ -1,20 +1,18 @@
 __author__ = 'Sahar'
 
 
-import numpy as np
 import pandas as pd
-import csv
 import re
 data = pd.read_csv("myData3.csv")
-K=data['cve.CVE_data_meta.ID'].values
+CVE_ID=data['cve.CVE_data_meta.ID'].values
 count=0
 key = 'CVE-2018-'
-CVEid = []
-for key in K:
+ID = []
+for key in CVE_ID:
     idNumber =0
     if len(key)>0:
         response = re.search(r'^(CVE-2018-)?(\d+)',key)
         idNumber = int(response.group(2))
-    CVEid.append(idNumber)
-CVEid
+    ID.append(idNumber)
+ID
 
