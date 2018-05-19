1389bcf18546ee4e2d3cf1bd27f4671f3db5ae0d
shfhm <37782726+shfhm@users.noreply.github.com>
2018-05-19 17:11:50 -0400

    Add files via upload

---
+++ CVEid.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+__author__ = 'Sahar'
+import numpy as np
+import pandas as pd
+import csv
+import re
+data = pd.read_csv("myData3.csv")
+K=data['cve.CVE_data_meta.ID'].values
+count=0
+key = 'CVE-2018-'
+CVEid = []
+for key in K:
+    idNumber =0
+    if len(key)>0:
+        response = re.search(r'^(CVE-2018-)?(\d+)',key)
+        idNumber = int(response.group(2))
+    CVEid.append(idNumber)
+CVEid
+
